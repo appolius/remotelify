66880633790e3407b9f20de3bf9801cee019477e
spehj <jurespeh10@gmail.com>
2025-07-22 13:36:08 +0200

    fix: update watch_reboot.sh to improve reboot trigger logging and file removal

--- watch_reboot.sh
+++ watch_reboot.sh
@@ -4,12 +4,13 @@ TRIGGER_FILE="/tmp/trigger-reboot"
 
 while true; do
   if [ -f "$TRIGGER_FILE" ]; then
-    echo "Reboot triggered by container"
-    rm "$TRIGGER_FILE"
+    echo "Reboot triggered by container at $(date)"
+    rm -f "$TRIGGER_FILE"
     reboot
   fi
   sleep 1
 done
+
 
 # chmod +x ~/watch_reboot.sh
 # ./watch_reboot.sh &
